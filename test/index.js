42b15adb97da33fbe7a1614e23cf1d1a99198608
Dave Benson <daveb@ffem.org>
2015-02-08 18:53:09 -0800

    revision of logging code

--- test/index.js
+++ test/index.js
@@ -1,4 +1,17 @@
 var log = require('../lib/litejsonlog.js');
-log.set_dir('test-litejsonlog');
+var fake_time = require('../lib/fake-time');
+log.configure({
+  dir: 'test-litejsonlog',
+  watcher: {
+    uploadFunction: function (uploadInfo, cb) {
+      console.log("uploadFunction run: " + uploadInfo);
+      cb(null);
+    }
+  }
+});
+var t = 404567890 * 3600 * 1000;                // some random hour boundary
+fake_time.initialize(t);
 log('foo',{bar:12});
+t += 2 * 3600 * 1000;
+fake_time.setTime(t);
 log.stop_rotation();
