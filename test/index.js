27a29a52ebd4627a64ea651f9cbbebdd636c14bb
Dave Benson <daveb@ffem.org>
2015-06-07 07:35:10 -0700

    fixes

--- test/index.js
+++ test/index.js
@@ -1,7 +1,10 @@
 var log = require('../lib/litejsonlog.js');
 var fake_time = require('../lib/fake-time');
+var common = require('../lib/common');
+
+var log_dir = 'test-litejsonlog-' + process.pid;
 log.configure({
-  dir: 'test-litejsonlog',
+  dir: log_dir,
   watcher: {
     uploadFunction: function (uploadInfo, cb) {
       console.log("uploadFunction run: " + uploadInfo);
@@ -16,3 +19,5 @@ t += 2 * 3600 * 1000;
 fake_time.setTime(t);
 log('foo',{bar:13});
 log.stopRotation();
+
+common.rmRecursiveSync(log_dir);
